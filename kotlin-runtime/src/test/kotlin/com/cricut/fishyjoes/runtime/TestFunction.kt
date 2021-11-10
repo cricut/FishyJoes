1b1549ec33e1bd4b09c82b77b0dec32fd2b2da3f
Andrew Cobb <acobb@cricut.com>
2021-11-10 15:49:40 -0700

    Prevent excessive copying in mutating methods. (#8)

--- kotlin-runtime/src/test/kotlin/com/cricut/fishyjoes/runtime/TestFunction.kt
+++ kotlin-runtime/src/test/kotlin/com/cricut/fishyjoes/runtime/TestFunction.kt
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
+data class SwiftReference(var ref: Long) {
+    external fun append(x: Long)
+    external fun addr(): Long
+    external fun log()
+}
+
 internal class TestFunction {
     @Test
     fun functionPassing() {
@@ -11,8 +17,24 @@ internal class TestFunction {
         assertEquals(13, fun1()(10, 3))
     }
 
+    @Test
+    fun copyOnWriteOK() {
+        val ref = makeReference()
+        var copyCount = 0
+        for (i in 0..1000) {
+            val oldAddress = ref.addr()
+            ref.append(i.toLong())
+            if (ref.addr() != oldAddress) {
+                copyCount += 1
+            }
+        }
+        println("$copyCount copies")
+        assertTrue(copyCount < 100)
+    }
+
     private external fun fun0(f: (Long, Long) -> Long): String
     private external fun fun1(): (Long, Long) -> Long
+    private external fun makeReference(): SwiftReference
 
     companion object {
         @BeforeAll
