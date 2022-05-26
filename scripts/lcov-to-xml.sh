8e61bed007b365a9d27eb6b29684f4d244d384c1
Andrew Cobb <acobb@cricut.com>
2022-05-26 15:13:40 -0600

    Add example lcov.info

--- scripts/lcov-to-xml.sh
+++ scripts/lcov-to-xml.sh
@@ -3,6 +3,28 @@
 set -euo pipefail
 
 # format documented at http://ltp.sourceforge.net/coverage/lcov/geninfo.1.php
+# e.g.
+
+# SF:/Users/acobb/src/FishyJoes/Sources/FishyJoesJavaRuntime/Box+Java.swift
+# FN:6,$s22FishyJoesCommonRuntime3BoxV0ab4JavaD0E04fromF0_3envACyxGs13OpaquePointerVSg_AD3EnvVtKFZ
+# <snip>
+# DA:6,3040
+# DA:7,3040
+# DA:8,3040
+# DA:15,3040
+# DA:16,3040
+# DA:17,3040
+# DA:18,3040
+# DA:20,0
+# DA:21,0
+# <snip>
+# end_of_record
+# SF:/Users/acobb/src/FishyJoes/Sources/FishyJoesJavaRuntime/Default.swift
+# <snip>
+# DA:12,2
+# <snip>
+# end_of_record
+
 sed -En -f <(cat <<'EOF'
 # outer wrapper start
 1 i\
