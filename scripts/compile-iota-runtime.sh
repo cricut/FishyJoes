45b714c365d3148e682e9ab21ca50e02f05ae7b4
Andrew Cobb <acobb@cricut.com>
2023-07-11 14:35:09 -0600

    build fat binary

--- scripts/compile-iota-runtime.sh
+++ scripts/compile-iota-runtime.sh
@@ -10,19 +10,14 @@ swift build --configuration $CONFIGURATION --product FishyJoesIotaRuntime
 binPath=$(swift build --configuration $CONFIGURATION --show-bin-path)
 
 mkdir -p c-sharp-runtime/runtimes/{osx,win,linux}/native
-mkdir -p dart-runtime/native
 
 {
     [[ -f $binPath/libFishyJoesIotaRuntime.dylib ]]
     cp $binPath/libFishyJoesIotaRuntime.dylib c-sharp-runtime/runtimes/osx/native/
-    cp $binPath/libFishyJoesIotaRuntime.dylib dart-runtime/native/
-    codesign -s i-hate-apple-security dart-runtime/native/libFishyJoesIotaRuntime.dylib
 } || {
     [[ -f $binPath/libFishyJoesIotaRuntime.dll ]]
     cp $binPath/FishyJoesIotaRuntime.dll c-sharp-runtime/runtimes/win/native/
-    cp $binPath/libFishyJoesIotaRuntime.dll dart-runtime/native/
 } || {
     [[ -f $binPath/libFishyJoesIotaRuntime.so ]]
     cp $binPath/libFishyJoesIotaRuntime.so c-sharp-runtime/runtimes/linux/native/
-    cp $binPath/libFishyJoesIotaRuntime.so dart-runtime/native/
 }
