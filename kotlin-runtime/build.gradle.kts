b9cbbacfb0ea25c61a83b2534745b865e1a43aae
kelly <kellyo@cricut.com>
2023-08-08 14:33:32 -0700

    ci: testing alpha version of android-swift-runtime

--- kotlin-runtime/build.gradle.kts
+++ kotlin-runtime/build.gradle.kts
@@ -119,7 +119,7 @@ tasks {
 }
 
 dependencies {
-    implementation("com.cricut:android-swift-runtime:0.1.0")
+    implementation("com.cricut:android-swift-runtime:0.1.1-alpha2")
     implementation(kotlin("stdlib:1.5.31"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
