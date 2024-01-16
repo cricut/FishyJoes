8a10e085fa1bc96037b4da25d5b3a9381f66ecf1
Matt Stoker <mstoker@cricut.com>
2024-01-16 10:46:34 -0700

    CI

--- scripts/compile-node-runtime.sh
+++ scripts/compile-node-runtime.sh
@@ -12,7 +12,7 @@ SKIP_LIPO="${SKIP_LIPO:-0}"
 
 # Swift does not properly read Windows "PATH" variable, instead trying to read "Path" only.
 # See: https://github.com/apple/swift-tools-support-core/issues/446
-PATH="$PATH;/c/Program Files/Git/usr/bin;/c/Program Files/Git/mingw64/libexec/git-core"
+PATH="/c/Program Files/Git/usr/bin;/c/Program Files/Git/mingw64/libexec/git-core;$PATH"
 [[ "${PATH:-}" ]] && export Path="$PATH"
 
 if [[ "$(uname -s)" == "Darwin" && $SKIP_LIPO == "0" ]]; then
