b96a789dacdf8fa7f09472acabdbe73b40f5d545
Matt Stoker <mstoker@cricut.com>
2024-01-16 17:13:35 -0700

    CI

--- scripts/compile-iota-runtime.sh
+++ scripts/compile-iota-runtime.sh
@@ -25,12 +25,9 @@ if [[ "$(uname -s)" == "Darwin" && $SKIP_LIPO == "0" ]]; then
 elif [[ "$(uname -s)" == *_NT* ]]; then
     # Swift does not properly read Windows "PATH" variable, instead trying to read "Path" only.
     # See: https://github.com/apple/swift-tools-support-core/issues/446
-    PATH="/c/Program Files/Git/usr/bin:/c/Program Files/Git/mingw64/libexec/git-core:$PATH"
-    [[ "${Path:-}" ]] && PATH="$PATH:$Path"
-    export Path="$PATH"
-
-    swift build "$@" --configuration "$CONFIGURATION" --product FishyJoesIotaRuntime
-    BIN_DIR="$(swift build --configuration "$CONFIGURATION" --show-bin-path)"
+    PATH="$PATH:/c/Program Files/Git/mingw64/libexec/git-core"
+    Path="$PATH" swift build "$@" --configuration "$CONFIGURATION" --product FishyJoesNodeRuntime
+    BIN_DIR="$(Path="$PATH" swift build --configuration "$CONFIGURATION" --show-bin-path)"
 else
     swift build "$@" --configuration "$CONFIGURATION" --product FishyJoesIotaRuntime
     BIN_DIR="$(swift build --configuration "$CONFIGURATION" --show-bin-path)"
