65422cc233d74dd78f372f27dde7e49e558b1ee4
Andrew Cobb <acobb@cricut.com>
2024-02-02 11:54:35 -0700

    Add tests for instance async methods

--- integration-tests/TestAPI-bindings/node-test/Methods.test.ts
+++ integration-tests/TestAPI-bindings/node-test/Methods.test.ts
@@ -32,72 +32,146 @@ test('instanceMethods', () => {
     expect(instance.instanceStored).toEqual(7654)
 });
 
-test('AsyncFunctionCall', async () => {
-    const value: number = await TestAPI.Methods.async42()
-    expect(value).toEqual(42)
+describe('AsyncInstanceTests', () => {
+    test('AsyncFunctionCall', async () => {
+        const value: number = await TestAPI.Methods.create().async42()
+        expect(value).toEqual(42)
+    })
+
+    test('AsyncYieldingFunctionCall', async () => {
+        const value: number = await TestAPI.Methods.create().asyncYield()
+        expect(value).toEqual(42)
+    })
+
+    test('AsyncSleepingFunctionCall', async () => {
+        const value: number = await TestAPI.Methods.create().asyncSleep()
+        expect(value).toEqual(42)
+    })
+
+    test('AsyncVoidFunctionCall', async () => {
+        const value: void = await TestAPI.Methods.create().asyncVoid()
+    })
+
+    test('AsyncCallbackFunctionCall0', async () => {
+        let threw: boolean = false
+        let value: number = 0
+        let ran: boolean = false
+        try {
+            value = await TestAPI.Methods.create().asyncCallbackFunc0(async () => {
+                ran = true
+                return 42
+            })
+        } catch (e) {
+            threw = true
+        }
+        expect(threw).toEqual(false)
+        expect(value).toEqual(42)
+        expect(ran).toEqual(true)
+    })
+
+    test('AsyncCallbackFunctionCallThrow', async () => {
+        let threw: boolean = false
+        let value: number = 0
+        let ran: boolean = false
+        try {
+            value = await TestAPI.Methods.create().asyncCallbackFunc0(async () => {
+                ran = true
+                throw 42
+            })
+        } catch (e) {
+            threw = true
+            expect(() => { throw e }).toThrowError(/42/)
+        }
+        expect(threw).toEqual(true)
+        expect(value).toEqual(0)
+        expect(ran).toEqual(true)
+    })
+
+    test('AsyncDoubleFunctionCall', async () => {
+        const value = await TestAPI.Methods.create().asyncDouble(1.0)
+        expect(value).toEqual(2.0)
+    })
+
+    test('AsyncMultipleArgsFunctionCall', async () => {
+        const value = await TestAPI.Methods.create().asyncMultipleArgs(1, async () => { return 2 })
+        expect(value).toEqual(3)
+    })
+
+    test('AsyncThrowingMethod', async () => {
+        const promise = TestAPI.Methods.create().asyncThrowing()
+        console.log(`promise = ${promise}`)
+        await expect(promise).rejects.toThrowError(/TheMethodError/)
+    })
 })
 
-test('AsyncYieldingFunctionCall', async () => {
-    const value: number = await TestAPI.Methods.asyncYield()
-    expect(value).toEqual(42)
-})
+describe('AsyncStaticTests', () => {
+    test('AsyncFunctionCall', async () => {
+        const value: number = await TestAPI.Methods.staticAsync42()
+        expect(value).toEqual(42)
+    })
 
-test('AsyncSleepingFunctionCall', async () => {
-    const value: number = await TestAPI.Methods.asyncSleep()
-    expect(value).toEqual(42)
-})
+    test('AsyncYieldingFunctionCall', async () => {
+        const value: number = await TestAPI.Methods.staticAsyncYield()
+        expect(value).toEqual(42)
+    })
 
-test('AsyncVoidFunctionCall', async () => {
-    const value: void = await TestAPI.Methods.asyncVoid()
-})
+    test('AsyncSleepingFunctionCall', async () => {
+        const value: number = await TestAPI.Methods.staticAsyncSleep()
+        expect(value).toEqual(42)
+    })
 
-test('AsyncCallbackFunctionCall0', async () => {
-    let threw: boolean = false
-    let value: number = 0
-    let ran: boolean = false
-    try {
-        value = await TestAPI.Methods.asyncCallbackFunc0(async () => {
-            ran = true
-            return 42
-        })
-    } catch (e) {
-        threw = true
-    }
-    expect(threw).toEqual(false)
-    expect(value).toEqual(42)
-    expect(ran).toEqual(true)
-})
+    test('AsyncVoidFunctionCall', async () => {
+        const value: void = await TestAPI.Methods.staticAsyncVoid()
+    })
 
-test('AsyncCallbackFunctionCallThrow', async () => {
-    let threw: boolean = false
-    let value: number = 0
-    let ran: boolean = false
-    try {
-        value = await TestAPI.Methods.asyncCallbackFunc0(async () => {
-            ran = true
-            throw 42
-        })
-    } catch (e) {
-        threw = true
-        expect(() => { throw e }).toThrowError(/42/)
-    }
-    expect(threw).toEqual(true)
-    expect(value).toEqual(0)
-    expect(ran).toEqual(true)
-})
+    test('AsyncCallbackFunctionCall0', async () => {
+        let threw: boolean = false
+        let value: number = 0
+        let ran: boolean = false
+        try {
+            value = await TestAPI.Methods.staticAsyncCallbackFunc0(async () => {
+                ran = true
+                return 42
+            })
+        } catch (e) {
+            threw = true
+        }
+        expect(threw).toEqual(false)
+        expect(value).toEqual(42)
+        expect(ran).toEqual(true)
+    })
 
-test('AsyncDoubleFunctionCall', async () => {
-    const value = await TestAPI.Methods.asyncDouble(1.0)
-    expect(value).toEqual(2.0)
-})
+    test('AsyncCallbackFunctionCallThrow', async () => {
+        let threw: boolean = false
+        let value: number = 0
+        let ran: boolean = false
+        try {
+            value = await TestAPI.Methods.staticAsyncCallbackFunc0(async () => {
+                ran = true
+                throw 42
+            })
+        } catch (e) {
+            threw = true
+            expect(() => { throw e }).toThrowError(/42/)
+        }
+        expect(threw).toEqual(true)
+        expect(value).toEqual(0)
+        expect(ran).toEqual(true)
+    })
 
-test('AsyncMultipleArgsFunctionCall', async () => {
-    const value = await TestAPI.Methods.asyncMultipleArgs(1, async () => { return 2 })
-    expect(value).toEqual(3)
-})
+    test('AsyncDoubleFunctionCall', async () => {
+        const value = await TestAPI.Methods.staticAsyncDouble(1.0)
+        expect(value).toEqual(2.0)
+    })
 
-test('AsyncThrowingMethod', async () => {
-    const promise = TestAPI.Methods.asyncThrowing()
-    console.log(`promise = ${promise}`)
-    await expect(promise).rejects.toThrowError(/TheMethodError/)
+    test('AsyncMultipleArgsFunctionCall', async () => {
+        const value = await TestAPI.Methods.staticAsyncMultipleArgs(1, async () => { return 2 })
+        expect(value).toEqual(3)
+    })
+
+    test('AsyncThrowingMethod', async () => {
+        const promise = TestAPI.Methods.staticAsyncThrowing()
+        console.log(`promise = ${promise}`)
+        await expect(promise).rejects.toThrowError(/TheMethodError/)
+    })
 })
