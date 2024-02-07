f1582c448c84e8bc10418ca71da0cebc08f90b9f
Doug Voss <dvoss@cricut.com>
2024-02-07 16:48:39 -0700

    add protocol test

---
+++ integration-tests/TestAPI-bindings/kotlin/src/test/kotlin/com/cricut/testapi/ProtocolTests.kt
@@ -0,0 +1,28 @@
+package com.cricut.testapi
+
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.lang.Exception
+
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+internal class ProtocolTests {
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            val procId = ProcessHandle.current().pid()
+            println("procId: $procId")
+            println()
+        }
+    }
+    @Test
+    fun testProtocols() {
+        val a = AProtocolImplementation(foo = "Foo", baz = true)
+        println("a.foo: ${a.foo}, baz: ${a.baz}")
+        val b = a.bar(x = 83, y = 128)
+        println("b.foo: ${b.foo}, b.baz: ${b.baz}")
+    }
+}
