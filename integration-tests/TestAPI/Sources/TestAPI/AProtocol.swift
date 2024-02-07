4702a66d9d19a992d8566252c2101f76ee3ea6cc
Doug Voss <dvoss@cricut.com>
2024-02-06 18:02:57 -0700

    Add AProtocol to test with

---
+++ integration-tests/TestAPI/Sources/TestAPI/AProtocol.swift
@@ -0,0 +1,35 @@
+/// <!-- FishyJoes.export(AProtocol) -->
+public protocol AProtocol {
+    /// <!-- FishyJoes.export(foo) -->
+    var foo: String { get set }
+
+    /// <!-- FishyJoes.export(bar) -->
+    func bar(x: Int, y: Int) -> AProtocol
+    
+    /// <!-- FishyJoes.export(baz) -->
+    var baz: Bool { get set }
+
+    /// <!-- FishyJoes.export(hasADefaultImplementation) -->
+    func hasADefaultImplementation() -> Int
+}
+
+extension AProtocol {
+    public func hasADefaultImplementation() -> Int { 42 }
+}
+
+/// <!-- FishyJoes.export(AProtocolImplementation) -->
+public struct AProtocolImplementation: AProtocol {
+    public var foo: String
+    public var baz: Bool
+
+    public init(foo: String, baz: Bool) {
+        self.foo = foo
+        self.baz = baz
+    }
+
+    // TODO: should we infer the export from protocol requirements?
+    /// <!-- FishyJoes.export(bar) -->
+    public func bar(x: Int, y: Int) -> AProtocol {
+        AProtocolImplementation(foo: "\(x + y)", baz: x == 2)
+    }
+}
