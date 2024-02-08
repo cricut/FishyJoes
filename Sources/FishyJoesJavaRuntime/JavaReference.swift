c2b36de249011aa04e7219813ba199b4fa2cd542
Doug Voss <dvoss@cricut.com>
2024-02-08 10:44:17 -0700

    Add currentThreadEnv

--- Sources/FishyJoesJavaRuntime/JavaReference.swift
+++ Sources/FishyJoesJavaRuntime/JavaReference.swift
@@ -4,7 +4,11 @@ import JNI
 
 public class JavaReference {
     public private(set) var object: jobject?
-    public let vm: UnsafeMutablePointer<JavaVM?>?
+    private let vm: UnsafeMutablePointer<JavaVM?>?
+    @inline(__always)
+    private var fns: JavaVM.Pointee {
+        vm!.pointee!.pointee
+    }
 
     public init(local: jobject?, env: Env) throws {
         self.object = try local.map { try env.globalRef($0) }
@@ -26,5 +30,22 @@ public class JavaReference {
             javaEnv.DeleteGlobalRef(object)
             self.object = nil
         }
+    }
+    
+    /// Gets or creates the JNI environment associated with the current thread
+    public func currentThreadEnv() throws -> Env {
+        var envRaw: UnsafeMutableRawPointer?
+
+        switch fns.GetEnv(vm, &envRaw, JNI_VERSION_1_4) {
+        case JNI_EDETACHED:
+            print("attaching thread \(Thread.current) to JVM")
+            let opaquePtr = OpaquePointer(envRaw)
+            var envUnraw = UnsafeMutablePointer<JNIEnv?>(opaquePtr)
+            try javaOk(fns.AttachCurrentThread(vm, &envUnraw, nil))
+        case let retCode:
+            try javaOk(retCode)
+        }
+        let env = UnsafeMutablePointer<JNIEnv?>(OpaquePointer(envRaw))
+        return Env(env: try javaNonNull(env))
     }
 }
