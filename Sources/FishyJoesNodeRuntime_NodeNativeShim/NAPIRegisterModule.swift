32d47ed743745ea3eb6556b7d2f027e1284e3ad4
Matt Stoker <mstoker@cricut.com>
2024-02-26 18:28:51 -0700

    Moved to Swift 5.9.1 across the full codebase

--- Sources/FishyJoesNodeRuntime_NodeNativeShim/NAPIRegisterModule.swift
+++ Sources/FishyJoesNodeRuntime_NodeNativeShim/NAPIRegisterModule.swift
@@ -1,5 +1,5 @@
-import NodeAPI
 import FishyJoesNodeRuntime
+import NodeAPI
 @available(*, deprecated, message: "Not actually deprecated, but this silences warnings because it may refer to deprecated methods")
 @_cdecl("napi_register_module_v1")
 public func napi_register_module_v1(env: napi_env, exports: napi_value) -> napi_value? {
