f201483775a3e6331037671558dfb6e5d43065be
Matt Stoker <mstoker@cricut.com>
2024-03-07 15:18:47 -0700

    MSVC still did not like pragma. Hopefully now...

--- integration-tests/TestAPI-bindings/Sources/Generated/NodeNativeShim/NAPIRegisterModule.c
+++ integration-tests/TestAPI-bindings/Sources/Generated/NodeNativeShim/NAPIRegisterModule.c
@@ -1,6 +1,6 @@
 #include <node_api.h>
 extern napi_value registerModuleTestAPI(napi_env env, napi_value exports);
-#pragma comment(linker, /export: napi_register_module_v1)
+#pragma comment(linker, "/export:napi_register_module_v1")
 napi_value napi_register_module_v1(napi_env env, napi_value exports) {
     return registerModuleTestAPI(env, exports);
 }
