156b2088a890a4c4adc472b1172571fb61b1f045
Doug Voss <dvoss@cricut.com>
2024-03-08 19:23:42 -0700

    Add the rest of test
    kind of like the ace of bass for for tests

--- integration-tests/TestAPI-bindings/dart/test/protocols_test.dart
+++ integration-tests/TestAPI-bindings/dart/test/protocols_test.dart
@@ -12,14 +12,21 @@ void main() {
   group('ProtocolTests', () {
       test('testProtocolImplementation', () {
         var a = AProtocolImplementation(foo: "Garply", baz: false);
-        // expect(a.foo, equals("Garply"));
-        // expect(a.baz, equals(false));
+        expect(a.foo, equals("Garply"));
+        expect(a.baz, equals(false));
 
-        // var b = a.bar(2, 128);
-        // expect(b.foo, equals("130"));
-        // expect(b.baz, equals(true));
+        var b = a.bar(2, 128);
+        expect(b.foo, equals("130"));
+        expect(b.baz, equals(true));
 
         expect(a.hasADefaultImplementation(9, -102.1), equals("-312 notBazzed"));
+        expect(a.hasADefaultImplementation(2, 0.345), equals("bazzy 3"));
+
+        expect(a.hasADefaultImplementation2("923.2185", true, 0.0898714), equals(3.7838466771424932E9));
+        expect(a.hasADefaultImplementation2("923.2185", false, 0.0898714), equals(1.9556754407899822E-5));
+
+        expect(a.hasADefaultImplementation2("0.9870923", true, 1.123123), equals(0.9589049888649063));
+        expect(a.hasADefaultImplementation2("0.9870923", false, 1.123123), equals(1.686253813623996));
       });
   });
 
