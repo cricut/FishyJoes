ea67bc4e28f83cdcd6b731d216bc46d671672fdd
Doug Voss <dvoss@cricut.com>
2024-03-13 16:27:07 -0600

    should not be committed

--- integration-tests/TestAPI-bindings/doIt.sh
+++
@@ -1,6 +0,0 @@
-#rm -f .build/arm64-apple-macosx/debug/libTestAPI.dylib
-rm -f .build/arm64-apple-macosx/debug/libTestAPI-iota.dylib
-/Applications/Xcode_15.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift-build --product TestAPI-iota --configuration debug -j 1 -Xlinker -rpath -Xlinker @loader_path
-#cp .build/arm64-apple-macosx/debug/libTestAPI.dylib kotlin/src/generated/resources/mac/libTestAPI.dylib
-cp .build/arm64-apple-macosx/debug/libTestAPI-iota.dylib dart/macos/native/libTestAPI-iota.dylib
-
