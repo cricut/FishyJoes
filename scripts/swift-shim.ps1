50797d8c4b88ef91fc093caf117551b78519c5d3
Andrew Cobb <acobb@cricut.com>
2024-03-22 11:35:52 -0600

    try more debug

--- scripts/swift-shim.ps1
+++ scripts/swift-shim.ps1
@@ -1,5 +1,8 @@
 #!/usr/bin/env powershell
 
+$ErrorActionPreference = "Stop"
+$DebugPreference = "Continue"
+
 # Swift does not properly read Windows "PATH" variable, instead trying to read "Path" only.
 # See: https://github.com/apple/swift-tools-support-core/issues/446
 # Also de-duplicate the path, as external tools seem to have a propensity to mangle the path
@@ -8,6 +11,8 @@ $CurrentPath = if ($env:Path -ne $null) { $env:Path } else { $env:PATH }
 $DeduplicatedPath = ($CurrentPath -split ';' | Select-Object -Unique) -join ';'
 $env:PATH = ''
 $env:Path = $DeduplicatedPath
+
+gci Env:PATH | Format-List
 
 # Perform execution and report errors encountered
 swift.exe @args
