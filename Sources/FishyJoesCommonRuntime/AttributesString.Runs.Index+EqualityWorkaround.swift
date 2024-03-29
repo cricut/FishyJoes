c95f4ef8b6cd4ec0b63cf14f71aedb0d1f5d91f5
Andrew Cobb <acobb@cricut.com>
2024-03-29 13:03:51 -0600

    typo

--- Sources/FishyJoesCommonRuntime/AttributesString.Runs.Index+EqualityWorkaround.swift
+++ Sources/FishyJoesCommonRuntime/AttributesString.Runs.Index+EqualityWorkaround.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension AttributedString.Runs.Index {
-    func workaroundEquals(rhs: AttributedString.Runs.Index) -> Bool {
-        !(self < rhs || rhs < self
+    public func workaroundEquals(_ rhs: AttributedString.Runs.Index) -> Bool {
+        !(self < rhs || rhs < self)
     }
 }
