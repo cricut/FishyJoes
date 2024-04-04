aab01940447bac1e8144b2ecca01b807e6f4c573
Doug Voss <dvoss@cricut.com>
2024-04-03 22:00:58 -0600

    Add placeholder test

---
+++ integration-tests/TestAPI-bindings/c-sharp/Cricut.TestAPI.Tests/ProtocolTests.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Cricut.TestAPI.Tests {
+    public class ProtocolTests {
+        [Fact]
+        public void testBasic(Console console) {
+            console.WriteLine("Well, hello");
+            Assert.Equal(9, 9);
+        }
+    }
+}
