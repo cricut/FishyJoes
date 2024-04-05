d770c0216f1425dccf094d963bcf94d68b0d96fc
Doug Voss <dvoss@cricut.com>
2024-04-04 20:00:53 -0600

    do more of the test thing

--- integration-tests/TestAPI-bindings/c-sharp/Cricut.TestAPI.Tests/ProtocolTests.cs
+++ integration-tests/TestAPI-bindings/c-sharp/Cricut.TestAPI.Tests/ProtocolTests.cs
@@ -5,8 +5,10 @@ using Xunit;
 namespace Cricut.TestAPI.Tests {
     public class ProtocolTests {
         [Fact]
-        public void testBasic() {
-            Console.WriteLine("Well, hello");
+        public void testAProtocol() {
+            var a = new AProtocolImplementation(Foo: "Haylo!", Baz: false);
+            System.Diagnostics.Debug.WriteLine($"a: {a}");
+            System.Diagnostics.Debug.WriteLine("Well, hello");
             Assert.Equal(9, 9);
         }
     }
