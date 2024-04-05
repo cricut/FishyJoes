03af3dc525e13518200e0576a2f1e02b320b2fb1
Doug Voss <dvoss@cricut.com>
2024-04-05 09:53:11 -0600

    Expose the lack of hookup for Bar
    __iota_TestAPI_AProtocolImplementation_bar doesn't make it over to the swift side

--- integration-tests/TestAPI-bindings/c-sharp/Cricut.TestAPI.Tests/ProtocolTests.cs
+++ integration-tests/TestAPI-bindings/c-sharp/Cricut.TestAPI.Tests/ProtocolTests.cs
@@ -6,10 +6,17 @@ namespace Cricut.TestAPI.Tests {
     public class ProtocolTests {
         [Fact]
         public void testAProtocol() {
+            int nProcessId = System.Diagnostics.Process.GetCurrentProcess().Id;
+            System.Diagnostics.Debug.WriteLine($"nProcessId: {nProcessId}");
+
             var a = new AProtocolImplementation(Foo: "Garply", Baz: false);
             Assert.Equal("Garply", a.Foo);
+            Assert.False(a.Baz);
+
             System.Diagnostics.Debug.WriteLine($"a: {a}");
             
+            var b = a.Bar(x: 2, y: 128);
+            System.Diagnostics.Debug.WriteLine($"b: {b}");
         }
     }
 }
