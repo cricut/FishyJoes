bc948c22f600ae679c4901dff322c3015923d07a
Doug Voss <dvoss@cricut.com>
2024-04-05 18:43:42 -0600

    Add mo tests

--- integration-tests/TestAPI-bindings/c-sharp/DebugMe/Program.cs
+++ integration-tests/TestAPI-bindings/c-sharp/DebugMe/Program.cs
@@ -6,6 +6,9 @@ using Cricut.FishyJoesRuntime;
 class Program {
 
     static async Task Main(string[] args) {
+        int nProcessId = System.Diagnostics.Process.GetCurrentProcess().Id;
+        System.Diagnostics.Debug.WriteLine($"nProcessId: {nProcessId}");
+
         Cricut.FishyJoesRuntime.Utilities.TrackAllocationsDebug = true;
 
         Loader.ensureLoaded();
@@ -13,6 +16,12 @@ class Program {
 
         var composed = AsyncFunctions.IntCompose(x => Task.FromResult(x + 1), x => Task.FromResult(x * 3));
         Console.WriteLine(await composed(3));
+        var a = new AProtocolImplementation(Foo: "Garply", Baz: false);            
+
+        System.Diagnostics.Debug.WriteLine($"a: {a}");
+
+        var b = a.Bar(x: 2, y: 128);
+        System.Diagnostics.Debug.WriteLine($"b: {b}");
 
         Console.WriteLine("");
         Console.WriteLine("TEARDOWN");
