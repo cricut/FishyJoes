cc97e20e9c29873e6f5029c48232cac718da81a8
Andrew Cobb <acobb@cricut.com>
2024-04-18 15:16:33 -0600

    url is string again

--- Sources/FishyJoesExecute/PackageDotSwiftDependency.swift
+++ Sources/FishyJoesExecute/PackageDotSwiftDependency.swift
@@ -4,7 +4,7 @@ enum PackageDotSwiftDependency {
     // This enum should be kept in sync with the one in the string in CodeGen.swift
     enum Dependency: Codable {
         case local(path: String)
-        case remote(url: URL, _ refSpec: RefSpec)
+        case remote(url: String, _ refSpec: RefSpec)
         enum RefSpec: Codable {
             case branch(name: String)
             case revision(name: String)
@@ -18,9 +18,9 @@ extension PackageDotSwiftDependency.Dependency {
         case let .fileSystem(_, path):
             self = .local(path: path)
         case let .sourceControl(_, url, .branch(names)) where names.count == 1:
-            self = .remote(url: url, .branch(name: names[0]))
+            self = .remote(url: url.formatted(), .branch(name: names[0]))
         case let .sourceControl(_, url, .exact(versions)) where versions.count == 1:
-            self = .remote(url: url, .revision(name: versions[0]))
+            self = .remote(url: url.formatted(), .revision(name: versions[0]))
         default:
             fatalError("ill-formed dependency (expected local, branch, or exact):\n  \(spmDependency)")
         }
