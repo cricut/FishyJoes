47f5ae4a394d860a1bb87012ffc7e9c66de97678
Doug Voss <dvoss@cricut.com>
2024-04-23 22:09:32 -0600

    Remove comments

--- integration-tests/TestAPI-bindings/node-test/Protocols.test.ts
+++ integration-tests/TestAPI-bindings/node-test/Protocols.test.ts
@@ -13,15 +13,4 @@ test('AProtocol', () => {
 
     expect(b.foo).toEqual("130")
     expect(b.baz).toEqual(true)
-
-    // let d = a.bar(1, 2)
-    // let e = d.foo
-    // console.log(`e: ${ e }`)
-
-    // let b = TestAPI.AProtocolImplementation.create()
-
-    // // console.log(`a: ${JSON.stringify(a)}`)
-    // //console.log(`b: ${JSON.stringify(b)}`)
-    // let c = b.foo
-    // console.log(`b.foo: ${ b.foo }`)
 });
