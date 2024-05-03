085d9ac5044601db7ec9bdcd8e05e1f53c4d3e1f
Doug Voss <dvoss@cricut.com>
2024-05-03 15:37:17 -0600

    noe more ?. needed
    Now that we have the Core interface and fromCore method

--- integration-tests/TestAPI-bindings/node-test/Protocols.test.ts
+++ integration-tests/TestAPI-bindings/node-test/Protocols.test.ts
@@ -12,14 +12,14 @@ test('testProtocolImplementation', () => {
     expect(b.foo).toEqual("130");
     expect(b.baz).toEqual(true)
 
-    expect(a.hasADefaultImplementation?.(9, -102.1)).toEqual("-312 notBazzed");
-    expect(a.hasADefaultImplementation?.(2, 0.345)).toEqual("bazzy 3");
+    expect(a.hasADefaultImplementation(9, -102.1)).toEqual("-312 notBazzed");
+    expect(a.hasADefaultImplementation(2, 0.345)).toEqual("bazzy 3");
 
-    expect(a.hasADefaultImplementation2?.("923.2185", true, 0.0898714)).toEqual(3.7838466771424932E9);
-    expect(a.hasADefaultImplementation2?.("923.2185", false, 0.0898714)).toEqual(1.9556754407899822E-5);
+    expect(a.hasADefaultImplementation2("923.2185", true, 0.0898714)).toEqual(3.7838466771424932E9);
+    expect(a.hasADefaultImplementation2("923.2185", false, 0.0898714)).toEqual(1.9556754407899822E-5);
 
-    expect(a.hasADefaultImplementation2?.("0.9870923", true, 1.123123)).toEqual(0.9589049888649063);
-    expect(a.hasADefaultImplementation2?.("0.9870923", false, 1.123123)).toEqual(1.686253813623996);
+    expect(a.hasADefaultImplementation2("0.9870923", true, 1.123123)).toEqual(0.9589049888649063);
+    expect(a.hasADefaultImplementation2("0.9870923", false, 1.123123)).toEqual(1.686253813623996);
 });
 
 test('testCore', () => {
@@ -274,7 +274,7 @@ async function testAsyncForeignSideFunctionsCore(a: TestAPI.TestAsyncFunctions) 
     sleep(1);
     expect(o).toEqual(3);
 
-    let p = await a.defaultExercise6?.(
+    let p = await a.defaultExercise6(
         async (a, b, c, d, e, f) => {
             return Math.floor(Number(a) + Number(b) + c + Number(d) + (await e()) + f);
         }
@@ -390,7 +390,7 @@ async function testAsyncSwiftSideFunctionsCore(a: TestAPI.TestAsyncFunctions) {
     sleep(1);
     expect(o).toEqual(97.4090910340281);
 
-    let p = await a.defaultExercise6?.(
+    let p = await a.defaultExercise6(
         async (a, b, c, d, e, f) => {
             return Math.floor(Number(a) + Number(b) + c + Number(d) + (await e()) + f);
         }
