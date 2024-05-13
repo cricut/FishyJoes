e95893be137b35bec6a646f620f17f0e5ed78468
Doug Voss <dvoss@cricut.com>
2024-05-13 17:53:02 -0600

    Don't spam the pid

--- integration-tests/TestAPI-bindings/node-test/pre_test.js
+++ integration-tests/TestAPI-bindings/node-test/pre_test.js
@@ -1,4 +1,6 @@
-console.log('pid: ' + process.pid)
+const debugPrints = false
+
+if (debugPrints) { console.log('pid: ' + process.pid) }
 
 // set breakpoint here to grab pid before proceeding with test so you can attach to it in Xcode
 const setBreakpoingHereToGrabPidBeforeProceedingWithTestSoYouCanAttachToItInXcode = 42;
