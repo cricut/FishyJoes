c01eee6d4148aa8634c70803e2758a68de8c4fe9
Doug Voss <dvoss@cricut.com>
2024-05-29 15:47:45 -0600

    Add field code analogous to the method code

--- Sources/FishyJoesCore/Field.swift
+++ Sources/FishyJoesCore/Field.swift
@@ -39,3 +39,96 @@ struct Field: Hashable {
         self.isAsync = isIsolated || variable.isAsync
     }
 }
+
+extension Field {
+    func isMostlyEqual(other: Field) -> Bool {
+        let nameMatches = name == other.name
+        // exportAnnotation may differ
+        let typeMatches = type == other.type
+        // documentation may differ
+        // definedIn may differ
+        let isStaticMatches = isStatic == other.isStatic
+        let isMutableMatches = isMutable == other.isMutable
+        let isPubliclyWritableMatches = isPubliclyWritable == other.isPubliclyWritable
+        let isThrowingMatches = isThrowing == other.isThrowing
+        // isComputed may differ
+        // isOverride may differ
+        let isAsyncMatches = isAsync == other.isAsync
+        // deprecation may differ
+        // isDefaultImplementation may differ
+
+        return nameMatches &&
+        typeMatches &&
+        isStaticMatches &&
+        isMutableMatches &&
+        isPubliclyWritableMatches &&
+        isThrowingMatches &&
+        isAsyncMatches
+    }
+    
+    enum FieldTypePreference {
+        case defaultImplementation
+        case normal
+    }
+    
+    static func fieldsPreferring(_ preference: FieldTypePreference, fields: [Field]) -> [Field] {
+        var preferredFields = [Field]()
+        for field in fields {
+            let mostlyEqualFields = preferredFields.filter {
+                return $0.isMostlyEqual(other: field)
+            }
+            if !mostlyEqualFields.isEmpty {
+                for mostlyEqualField in mostlyEqualFields {
+                    let useMostlyEqualField = preference == .defaultImplementation ? mostlyEqualField.isDefaultImplementation : !mostlyEqualField.isDefaultImplementation
+
+                    if useMostlyEqualField {
+                        guard let index = preferredFields.firstIndex(of: field) else {
+                            assertionFailure("field should exist in preferredFields")
+                            continue
+                        }
+                        preferredFields.remove(at: index)
+                        preferredFields.insert(mostlyEqualField, at: index)
+                    } else { // use field
+                        guard let index = preferredFields.firstIndex(of: mostlyEqualField) else {
+                            assertionFailure("mostlyEqualField should exist in preferredFields")
+                            continue
+                        }
+                        preferredFields.remove(at: index)
+                        preferredFields.insert(field, at: index)
+                    }
+                }
+            } else {
+                preferredFields.append(field)
+            }
+        }
+        return preferredFields
+    }
+
+    static func fields(type: Type) -> [Field] {
+        var defaultFields = [Field]()
+        let protocols = type.implements.values.compactMap { $0 as? SourceryProtocol }
+        for prot in protocols {
+            let protDefaultFields = prot.rawVariables.compactMap {
+                if $0.definedInType?.isExtension == true {
+                    return Field($0, type: prot, isDefaultImplementation: true)
+                } else {
+                    return nil
+                }
+            }
+
+            defaultFields.append(contentsOf: protDefaultFields)
+        }
+
+        let isDefinedInProtocol = type is SourceryProtocol
+        let normalFields = type.variables.compactMap {
+            if $0.definedInType?.isExtension == true {
+                return Field($0, type: type, isDefaultImplementation: type is SourceryProtocol)
+            } else {
+                return nil
+            }
+        }
+
+        let fields = Field.fieldsPreferring(.normal, fields: normalFields + defaultFields)
+        return fields
+    }
+}
