3c2a3159453282bf85c9f9b8456a55b5cf769150
Doug Voss <dvoss@cricut.com>
2024-06-04 10:08:54 -0600

    remove cruft

--- integration-tests/TestAPI-bindings/dart/test/class_test.dart
+++ integration-tests/TestAPI-bindings/dart/test/class_test.dart
@@ -28,57 +28,5 @@ void main() {
 
         expect(a1.toString(), isNot(equals(b1.toString())));
       });
-
-      test('testConstruction', () {
-          final memberwise = Structs_MemberwiseStruct.create();
-          expect(memberwise.immutable, equals("Eternal"));
-          expect(memberwise.mutable, equals("Fickle"));
-
-          final reference = Structs_ReferenceStruct.create();
-          expect(reference.immutable, equals("Eternal"));
-          expect(reference.mutable, equals("Fickle"));
-      });
-
-      test('testMutablility', () {
-          final memberwise = Structs_MemberwiseStruct.create();
-          expect(memberwise.immutable, equals("Eternal"));
-          // TODO: Ensure this is true!
-          // memberwise.immutable = "Should generate a compile Error!";
-          expect(memberwise.mutable, equals("Fickle"));
-          memberwise.mutable = "Fickle as the wind";
-          expect(memberwise.mutable, isNot(equals("Fickle")));
-
-          final reference = Structs_ReferenceStruct.create();
-          expect(reference.immutable, equals("Eternal"));
-          // TODO: Ensure this is true!
-          // reference.immutable = "Should generate a compile Error!";
-          expect(reference.mutable, equals("Fickle"));
-          memberwise.mutable = "Fickle as the wind";
-          expect(memberwise.mutable, isNot(equals("Fickle")));
-      });
-
-      test('testEquality', () {
-          expect(Structs_MemberwiseStruct.create(), equals(Structs_MemberwiseStruct.create()));
-          expect(Structs_ReferenceStruct.create(), equals(Structs_ReferenceStruct.create()));
-          expect(Structs_MemberwiseStruct.create(), isNot(equals(Structs_ReferenceStruct.create())));
-      });
-
-      test('MutatingFunctions', () async {
-          final struct = Structs_MutableStruct.create();
-          expect(struct.i, equals(0));
-          struct.increment();
-          expect(struct.i, equals(1));
-          await struct.incrementAsync();
-          expect(struct.i, equals(2));
-      });
-
-      test('AsyncMemberFunctions', () async {
-          final memberwise = Structs_MemberwiseStruct.create();
-          expect(await memberwise.asyncGetMutable(), equals("Fickle"));
-          final reference = Structs_ReferenceStruct.create();
-          expect(await reference.asyncGetMutable(), equals("Fickle"));
-          final mutable = Structs_MutableStruct.create();
-          expect(await mutable.asyncGetI(), equals(0));
-      });
   });
 }
