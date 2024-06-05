ddb6ebc354f6493f3916c760b03e9896d81d072d
Andrew Cobb <acobb@cricut.com>
2024-06-05 15:04:14 -0600

    fix typo

--- Sources/FishyJoesCommonRuntime/Utilities.swift
+++ Sources/FishyJoesCommonRuntime/Utilities.swift
@@ -3,7 +3,7 @@
 @inline(__always)
 public func silenceTryWarning<T>(_ x: T) throws -> T { x }
 
-// Used to silence warnings in generated code where a "va" may or may not have mutations applied to it
+// Used to silence warnings in generated code where a "var" may or may not have mutations applied to it
 @inlinable
 @inline(__always)
 public func silenceMutationWarning<T>(_ x: inout T) {}
