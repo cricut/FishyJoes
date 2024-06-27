6d15b5ef5c06e3a16e75532757d56e52de845259
Doug Voss <dvoss@cricut.com>
2024-06-27 14:43:31 -0600

    remove todo

--- Sources/FishyJoesCore/TranslatedTypes/TranslatedStruct.swift
+++ Sources/FishyJoesCore/TranslatedTypes/TranslatedStruct.swift
@@ -101,7 +101,6 @@ struct TranslatedStruct: TranslatedType {
         fragment.outputBlock("extension \(sourceType.name): NodeMutator {") {
             fragment.output("public typealias SwiftType = Self")
             fragment.outputBlock("public static func fromNode(_ value: NAPI.Value, env: NAPI.Env) throws -> Self {") {
-                // TODO: type check
                 fragment.outputBlock("Self(") {
                     for (index, storedVar) in storedVariables.enumerated() {
                         let resolved = context.resolve(type: storedVar.type)
