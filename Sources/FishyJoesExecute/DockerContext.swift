fcd4eda2561262c5b8364cc9c97a73d66d435004
Andrew Cobb <acobb@cricut.com>
2024-07-12 16:28:34 -0600

    update CI

--- Sources/FishyJoesExecute/DockerContext.swift
+++ Sources/FishyJoesExecute/DockerContext.swift
@@ -5,7 +5,7 @@ struct DockerContext {
     var hostDockerBinary: String
     let passGitAuth: Bool
 
-    var image = "ghcr.io/cricut/android-swift-runtime:2.0.0"
+    var image = "ghcr.io/cricut/android-swift-runtime:2.0.1"
     var platform = "linux/amd64"
     var mountMappings: [String: String]
     var startDirectory: String
