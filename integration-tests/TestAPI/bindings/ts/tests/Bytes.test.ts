923181e00e74189a20f2ba7b77ca2f964cc84252
Andrew Cobb <acobb@cricut.com>
2025-09-18 17:33:09 -0600

    apparently typescript has gotten marginally less bad

--- integration-tests/TestAPI/bindings/ts/tests/Bytes.test.ts
+++ integration-tests/TestAPI/bindings/ts/tests/Bytes.test.ts
@@ -16,43 +16,42 @@ test('BytesEcho', () => {
         .toEqual(arrayFrom(byteArray.buffer));
 });
 
-// typescript thinks node Buffers are ArrayBuffers for some reason, so test that that works anyway
+// (old versions of) typescript think node Buffers are ArrayBuffers for some reason, so test that that works anyway
 test('ConsumeNodeBuffer', () => {
-
-    const buffer = Buffer.alloc(8, 'na-');
+    const buffer = Buffer.alloc(8, 'na-') as any;
     expect(arrayFrom(TestAPI.Bytes.echoData(buffer)))
         .toEqual([110, 97, 45, 110, 97, 45, 110, 97]);
 });
 
-// typescript also thinks Uint8Arrays are ArrayBuffers
+// (old versions of) typescript also think Uint8Arrays are ArrayBuffers
 test('ConsumeUint8Array', () => {
-    const u8Array = Uint8Array.from([1, 2, 3, 6, 11, 23, 47, 106, 235]);
+    const u8Array = Uint8Array.from([1, 2, 3, 6, 11, 23, 47, 106, 235]) as any;
     expect(arrayFrom(TestAPI.Bytes.echoData(u8Array)))
         .toEqual([1, 2, 3, 6, 11, 23, 47, 106, 235]);
 });
 
 test('ConsumeInt8Array', () => {
-    const i8Array = Int8Array.from([1, 2, 3, 6, 11, 23, 47, 106, -21]);
+    const i8Array = Int8Array.from([1, 2, 3, 6, 11, 23, 47, 106, -21]) as any;
     expect(arrayFrom(TestAPI.Bytes.echoData(i8Array)))
         .toEqual([1, 2, 3, 6, 11, 23, 47, 106, 235]);
 });
 
 test('ConsumeUint8ClampedArray', () => {
-    let u8cArray = Uint8ClampedArray.from([1, 2, 3, 6, 11, 23, 47, 106, 235]);
+    let u8cArray = Uint8ClampedArray.from([1, 2, 3, 6, 11, 23, 47, 106, 235]) as any;
     u8cArray = new Uint8ClampedArray(u8cArray.buffer, 1, 4);
     expect(arrayFrom(TestAPI.Bytes.echoData(u8cArray)))
         .toEqual([2, 3, 6, 11]);
 });
 
 test('ConsumeUint16Array', () => {
-    let u16Array = Uint16Array.from([1, 1, 1, 2, 3, 6, 11]);
+    let u16Array = Uint16Array.from([1, 1, 1, 2, 3, 6, 11]) as any;
     u16Array = new Uint16Array(u16Array.buffer, 4);
     expect(arrayFrom(TestAPI.Bytes.echoData(u16Array)))
         .toEqual([1, 0, 2, 0, 3, 0, 6, 0, 11, 0]);
 });
 
 test('ConsumeBigUint64Array', () => {
-    let u16Array = BigUint64Array.from([BigInt("0xdecafbadc0ffee")]);
+    let u16Array = BigUint64Array.from([BigInt("0xdecafbadc0ffee")]) as any;
     expect(arrayFrom(TestAPI.Bytes.echoData(u16Array)))
         .toEqual([0xee, 0xff, 0xc0, 0xad, 0xfb, 0xca, 0xde, 0x00]);
 });
