5aba0ce3458ca86ba3ad502d9438017c4758797e
Andrew Cobb <acobb@cricut.com>
2026-03-18 10:15:56 -0600

    make sure licenses are present in source for distribution

--- Sources/FishyJoesNodeRuntime/Templates/wasm-napi.js
+++ Sources/FishyJoesNodeRuntime/Templates/wasm-napi.js
@@ -1,6 +1,29 @@
 'use strict';
 
-// adapted from https://github.com/devsnek/napi-wasi-experiments/blob/main/napi.js
+// Adapted from https://github.com/devsnek/napi-wasi-experiments/blob/main/napi.js
+// Additions copyright (c) 2026 Cricut Inc.
+
+// Original copyright and license:
+
+// Copyright (c) 2021 Gus Caplan
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 
 // const vm = require('vm');
 import { Buffer } from 'buffer';
